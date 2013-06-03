a670bcafdda55697a3f43a899e2de78027fddf5a
Kyle Lacy <kylewlacy@me.com>
2013-06-03 05:59:36 -0700

    Add unit tests for Address class

---
+++ Electrolyte.Test/AddressTest.cs
@@ -0,0 +1,25 @@
+using System;
+using NUnit.Framework;
+
+namespace Electrolyte.Test {
+	[TestFixture]
+	public class AddressTest {
+		[Test]
+		public void AddressFormatTest() {
+			Assert.DoesNotThrow(() => {
+				new Address("1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp");
+				new Address("31uEbMgunupShBVTewXjtqbBv5MndwfXhb");
+				new Address("1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62i");
+			});
+
+			Assert.Throws<InvalidAddressException>(() => { new Address("2AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62i"); });
+			Assert.Throws<InvalidAddressException>(() => { new Address("AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62i"); });
+			Assert.Throws<InvalidAddressException>(() => { new Address("1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62I"); });
+			Assert.Throws<InvalidAddressException>(() => { new Address("2AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62O"); });
+			Assert.Throws<InvalidAddressException>(() => { new Address("2AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62l"); });
+			Assert.Throws<InvalidAddressException>(() => { new Address("1AGN"); });
+			Assert.Throws<InvalidAddressException>(() => { new Address("1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62iiiiiiiiiiii"); });
+		}
+	}
+}
+
