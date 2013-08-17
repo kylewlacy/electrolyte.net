7a181a3316ee9696e4e8d893d10a45d1950c153d
Kyle Lacy <kylewlacy@me.com>
2013-08-17 04:44:12 -0700

    Update CLI program example
    Until a means of unit testing network communication is stubbed, using the CLI will suffice in the meantime

--- Electrolyte.CLI/Program.cs
+++ Electrolyte.CLI/Program.cs
@@ -1,15 +1,26 @@
 using System;
-using Electrolyte;
+using System.Collections.Generic;
+using Electrolyte.Messages;
+using Electrolyte.Networking;
 
 namespace Electrolyte.CLI {
 	class MainClass {
 		public static void Main(string[] args) {
+//			Console.Write("Enter a transaction: ");
+//			string hex = Console.ReadLine();
+//
+//			Console.Write("Enter tx height: ");
+//			ulong height = UInt64.Parse(Console.ReadLine());
+//
+//			Transaction tx = protocol.GetTransaction(hex, height);
+//
+//			Console.WriteLine("First output script pubkey is:\n{0}", tx.Outputs[0].ScriptPubKey);
+
 			Console.Write("Enter an address: ");
 			Address address = new Address(Console.ReadLine());
 
-			Console.WriteLine("Address is {0}", address.ID);
-
-
+			List<Transaction> txHistory = Network.GetAddressHistory(address);
+			Console.WriteLine("Address {0} has {1} transactions", address, txHistory.Count);
 		}
 	}
 }
