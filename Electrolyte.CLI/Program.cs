8295652b0e253d7e619fab2498c724ed20820f9d
Kyle Lacy <kylewlacy@me.com>
2013-08-18 01:16:07 -0700

    Update CLI testbed

--- Electrolyte.CLI/Program.cs
+++ Electrolyte.CLI/Program.cs
@@ -16,11 +16,36 @@ namespace Electrolyte.CLI {
 //
 //			Console.WriteLine("First output script pubkey is:\n{0}", tx.Outputs[0].ScriptPubKey);
 
+
+
+			// [<network prefix>] + ScriptPubKey["pubKeyHash"].HexToByteArray() == Sender Address bytes
+			// [<network prefix>] + ripemd(sha25(ScriptSig["pubKey"])) == Recipient Address bytes
+
+
+//			Console.Write("Enter an address: ");
+//			Address address = new Address(Console.ReadLine());
+//
+//			List<Transaction> txHistory = Network.GetAddressHistory(address);
+//			Console.WriteLine("Address {0} has {1} transactions", address, txHistory.Count);
+//			Console.WriteLine("==================");
+//
+//			foreach(Transaction tx in txHistory) {
+//				Console.WriteLine("{0}", tx.Hash);
+//				Console.WriteLine("  Senders");
+//				foreach(Address sender in tx.Senders) {
+//					Console.WriteLine("    {0}", sender);
+//				}
+//
+//				Console.WriteLine("  Recipients");
+//				foreach(Address recipient in tx.Recipients) {
+//					Console.WriteLine("    {0}", recipient);
+//				}
+//			}
+
 			Console.Write("Enter an address: ");
 			Address address = new Address(Console.ReadLine());
 
-			List<Transaction> txHistory = Network.GetAddressHistory(address);
-			Console.WriteLine("Address {0} has {1} transactions", address, txHistory.Count);
+			Console.WriteLine("Balance: {0}", Network.GetAddressBalance(address));
 		}
 	}
 }
