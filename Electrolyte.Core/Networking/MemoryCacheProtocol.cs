fc154db4a7876bd48822a17387db211dcd6c02a0
Kyle Lacy <kylewlacy@me.com>
2013-09-01 09:42:50 -0700

    Fix race conditions in `MemoryCacheProtocol`

--- Electrolyte.Core/Networking/MemoryCacheProtocol.cs
+++ Electrolyte.Core/Networking/MemoryCacheProtocol.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using Electrolyte.Messages;
@@ -28,33 +29,48 @@ namespace Electrolyte.Networking {
 		public static TimeSpan MaxExchangeRateAge = new TimeSpan(0, 5, 0);
 
 		Dictionary<string, Transaction> TransactionCache = new Dictionary<string, Transaction>();
+		static readonly SemaphoreSlim txCacheLock = new SemaphoreSlim(1);
+
 		Dictionary<Money.CurrencyType, Dictionary<Money.CurrencyType, ExchangeRateInfo>> ExchangeRateCache = new Dictionary<Money.CurrencyType, Dictionary<Money.CurrencyType,ExchangeRateInfo>>();
+		static readonly SemaphoreSlim exchangeRateLock = new SemaphoreSlim(1);
 
 		public async override Task<Transaction> GetTransactionAsync(TransactionInfo info) {
-			if(!TransactionCache.ContainsKey(info.Hex))
-				TransactionCache.Add(info.Hex, await base.GetTransactionAsync(info));
+			await txCacheLock.WaitAsync();
+			try {
+				if(!TransactionCache.ContainsKey(info.Hex))
+					TransactionCache.Add(info.Hex, await base.GetTransactionAsync(info));
+			}
+			finally {
+				txCacheLock.Release();
+			}
 
 			return TransactionCache[info.Hex];
 		}
 
 		public override async Task<decimal> GetExchangeRateAsync(Money.CurrencyType c1, Money.CurrencyType c2) {
-			if(ExchangeRateCache.ContainsKey(c2)) {
-				return 1m / (await GetExchangeRateAsync(c2, c1));
+			await exchangeRateLock.WaitAsync();
+			try {
+				if(ExchangeRateCache.ContainsKey(c2)) {
+					return 1m / (await GetExchangeRateAsync(c2, c1));
+				}
+
+				if(!ExchangeRateCache.ContainsKey(c1))
+					ExchangeRateCache.Add(c1, new Dictionary<Money.CurrencyType, ExchangeRateInfo>());
+
+				if(ExchangeRateCache[c1].ContainsKey(c2)) {
+					if(ExchangeRateCache[c1][c2].Age <= MaxExchangeRateAge)
+						return ExchangeRateCache[c1][c2].Rate;
+
+					ExchangeRateCache[c1].Remove(c2);
+				}
+
+				decimal rate = await base.GetExchangeRateAsync(c1, c2);
+				ExchangeRateCache[c1].Add(c2, new ExchangeRateInfo(rate));
+				return rate;
 			}
-
-			if(!ExchangeRateCache.ContainsKey(c1))
-				ExchangeRateCache.Add(c1, new Dictionary<Money.CurrencyType, ExchangeRateInfo>());
-
-			if(ExchangeRateCache[c1].ContainsKey(c2)) {
-				if(ExchangeRateCache[c1][c2].Age <= MaxExchangeRateAge)
-					return ExchangeRateCache[c1][c2].Rate;
-
-				ExchangeRateCache[c1].Remove(c2);
+			finally {
+				exchangeRateLock.Release();
 			}
-
-			decimal rate = await base.GetExchangeRateAsync(c1, c2);
-			ExchangeRateCache[c1].Add(c2, new ExchangeRateInfo(rate));
-			return rate;
 		}
 	}
 }
