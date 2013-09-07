40c3ba73f549bc2ead7664ba8889dc0ad3945a22
Kyle Lacy <kylewlacy@me.com>
2013-09-07 14:57:04 -0700

    Add `Transaction.Delta.ToString()` override

--- Electrolyte.Core/Messages/Transaction.Structs.cs
+++ Electrolyte.Core/Messages/Transaction.Structs.cs
@@ -29,6 +29,10 @@ namespace Electrolyte.Messages {
 				Transaction = tx;
 				Value = value;
 			}
+
+			public override string ToString() {
+				return String.Format("{0}{1}", Value > Money.Zero("BTC") ? "+" : "", Value.ToString());
+			}
 		}
 	}
 }
