a3d8c5a17b46f49e33a9ce384d8a3f352be16577
Kyle Lacy <kylewlacy@me.com>
2013-09-28 10:34:44 -0700

    Eliminate dependency on `System.Net.Sockets`

--- Electrolyte.Core/Networking/ElectrumProtocol.cs
+++ Electrolyte.Core/Networking/ElectrumProtocol.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Linq;
-using System.Net.Sockets;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
